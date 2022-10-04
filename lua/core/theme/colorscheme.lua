32e260e2ecbe12580a84587fd1900a9cdeaef528
Zong,xin <zongxin19930430@163.com>
2022-10-03 23:00:53 -0700

    init commit

--- lua/core/theme/colorscheme.lua
+++ lua/core/theme/colorscheme.lua
@@ -1,8 +1,9 @@
-vim.cmd [[
-try
-  colorscheme darkplus
-catch /^Vim\%((\a\+)\)\=:E185/
-  colorscheme default
-  set background=dark
-endtry
-]]
+-- vim.cmd [[
+-- try
+  -- colorscheme darkplus
+-- catch /^Vim\%((\a\+)\)\=:E185/
+  -- colorscheme default
+  -- set background=dark
+-- endtry
+-- ]]
+-- Theme
--- lua/plugin/comment.lua
+++ lua/plugin/comment.lua
@@ -4,6 +4,26 @@ if not status_ok then
 end
 
 comment.setup {
+  toggler = {
+      -- 切换行注释
+      line = "gcc",
+      --- 切换块注释
+      block = "gCC"
+    },
+    opleader = {
+      -- 可视模式下的行注释
+      line = "gc",
+      -- 可视模式下的块注释
+      block = "gC"
+    },
+    extra = {
+      -- 在当前行上方新增行注释
+      above = "gcO",
+      -- 在当前行下方新增行注释
+      below = "gco",
+      -- 在当前行行尾新增行注释
+      eol = "gcA"
+    },
   pre_hook = function(ctx)
     local U = require "Comment.utils"
 
