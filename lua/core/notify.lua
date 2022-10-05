ae78ca40102584df6c58668cb53ff8a3e291b645
zongxin1993 <36766561+zongxin1993@users.noreply.github.com>
2022-10-05 21:21:06 +0800

    Update notify.lua

--- lua/core/notify.lua
+++ lua/core/notify.lua
@@ -47,6 +47,12 @@ function M.setup()
 
   notify.setup(opts)
   vim.notify = notify
+  vim.notify = function(msg, ...)
+    if msg:match("warning: multiple different client offset_encodings") then
+        return
+    end
+    notify(msg, ...)
+  end
 end
 
 return M
